1c03b052397839202da8a56f3c4f85a424bc6b28
Allan Kinyua Murigi <allan.murigi@student.moringaschool.com>
2023-10-22 05:48:07 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+function writeCards(names, eventName){
+    const surprise = [];
+    for(let i=0; i < names.length; i++){
+        const message =`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`
+        surprise.push(message);
+    }
+    return surprise;
+}
+const messages = writeCards(["Guadalupe","Ollie","Aki"], "surprise")
+console.log(messages)
+
+//countdown
+function countDown(starter){
+    while (starter >= 0){
+        console.log(starter)
+        starter--
+    }
+}
+  countDown(10)
+  
